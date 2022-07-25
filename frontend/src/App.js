6c17a38910275fbd45b94cde3087bb330f948005
vanutcho123 <hainguyen.061001@gmail.com>
2022-07-25 10:16:05 +0700

    8. Add bootstrap UI Framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
+import Container from "react-bootstrap/esm/Container";
 
 function App() {
   return (
@@ -13,11 +14,18 @@ function App() {
       <div className="app">
         <Header />
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:slug" element={<Product />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:slug" element={<Product />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">
+            Create by NGUYENVANHAI| 2021 All Right reserved.
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
-    <div className="header">
-      <Link to="/">H2SHOP</Link>
-    </div>
+    <header className="header">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>H2SHOP</Navbar.Brand>
+          </LinkContainer>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
